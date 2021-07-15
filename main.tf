e2929333cd5b3d3e1797cb9f6299dcb88bff74d8
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-15 16:11:23 +0900

    feat: Add Multi AZs
    1. ap-northeast-2의 a와 c로 분리
    2. Public, AP, DB의 Subnet을 각각 2개로 분리. 총 6개 Subnet
    3. NAT Gateway를 각 AZ에 추가
    4. Internal Route Table에 NAT Gateway추가

--- main.tf
+++ main.tf
@@ -27,32 +27,67 @@ resource "aws_vpc" "main_vpc" {
 }
 #############################################
 ################# Subnet ####################
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_a" {
   vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.public_subnet
+  cidr_block = var.public_subnet_a
+  availability_zone = var.az_a
 
   tags = {
-    Name = "70491_public_subnet"
+    Name = "70491_public_subnet_a"
   }
 }
 
-resource "aws_subnet" "ap_private_subnet" {
+resource "aws_subnet" "public_subnet_c" {
   vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.ap_private_subnet
+  cidr_block = var.public_subnet_c
+  availability_zone = var.az_c
 
   tags = {
-    Name = "70491_ap_private_subnet"
+    Name = "70491_public_subnet_c"
   }
 }
 
-resource "aws_subnet" "db_private_subnet" {
+
+resource "aws_subnet" "ap_private_subnet_a" {
   vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.db_private_subnet
+  cidr_block = var.ap_private_subnet_a
+  availability_zone = var.az_a
 
   tags = {
-    Name = "70491_db_private_subnet"
+    Name = "70491_ap_private_subnet_a"
   }
 }
+
+resource "aws_subnet" "ap_private_subnet_c" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = var.ap_private_subnet_c
+  availability_zone = var.az_c
+
+  tags = {
+    Name = "70491_ap_private_subnet_c"
+  }
+}
+
+resource "aws_subnet" "db_private_subnet_a" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = var.db_private_subnet_a
+  availability_zone = var.az_a
+
+  tags = {
+    Name = "70491_db_private_subnet_a"
+  }
+}
+
+resource "aws_subnet" "db_private_subnet_c" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = var.db_private_subnet_c
+  availability_zone = var.az_c
+
+  tags = {
+    Name = "70491_db_private_subnet_c"
+  }
+}
+
 #############################################
 ########## Internet Gateway #################
 resource "aws_internet_gateway" "igw" {
@@ -60,6 +95,30 @@ resource "aws_internet_gateway" "igw" {
 
   tags = {
     Name = "70491_igw"
+  }
+}
+#############################################
+############### NAT Gateway #################
+resource "aws_eip" "nat_ip_a" {
+  vpc = true
+}
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_ip_a.id
+  connectivity_type = "public"
+  subnet_id = aws_subnet.public_subnet_a.id
+  tags = {
+    Name = "70491_nat_a"
+  }
+}
+resource "aws_eip" "nat_ip_c" {
+  vpc = true
+}
+resource "aws_nat_gateway" "nat_c" {
+  allocation_id = aws_eip.nat_ip_c.id
+  connectivity_type = "public"
+  subnet_id = aws_subnet.public_subnet_c.id
+  tags = {
+    Name = "70491_nat_c"
   }
 }
 #############################################
@@ -77,8 +136,7 @@ resource "aws_route_table" "internal_rt" {
 
   tags = {
     Name = "70491_internal_rt"
-  }
-  
+  } 
 }
 #############################################
 ################# Route #####################
@@ -87,31 +145,40 @@ resource "aws_route" "ext_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
+resource "aws_route" "int_route_nat_a" {
+  route_table_id = aws_route_table.internal_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat_a.id
+}
+resource "aws_route" "int_route_nat_c" {
+  route_table_id = aws_route_table.internal_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat_c.id
+}
 #############################################
 ########### Route Table Association #########
-resource "aws_route_table_association" "ext_rt_subnet" {
-  subnet_id = aws_subnet.public_subnet.id
+resource "aws_route_table_association" "ext_rt_subnet_a" {
+  subnet_id = aws_subnet.public_subnet_a.id
   route_table_id = aws_route_table.external_rt.id
 }
-resource "aws_route_table_association" "int_rt_subnet_ap" {
-  subnet_id = aws_subnet.ap_private_subnet.id
+resource "aws_route_table_association" "ext_rt_subnet_c" {
+  subnet_id = aws_subnet.public_subnet_c.id
+  route_table_id = aws_route_table.external_rt.id
+}
+resource "aws_route_table_association" "int_rt_subnet_ap_a" {
+  subnet_id = aws_subnet.ap_private_subnet_a.id
   route_table_id = aws_route_table.internal_rt.id
 }
-resource "aws_route_table_association" "int_rt_subnet_db" {
-  subnet_id = aws_subnet.db_private_subnet.id
+resource "aws_route_table_association" "int_rt_subnet_ap_c" {
+  subnet_id = aws_subnet.ap_private_subnet_c.id
   route_table_id = aws_route_table.internal_rt.id
 }
-#############################################
-############### NAT Gateway #################
-resource "aws_eip" "nat_ip" {
-  vpc = true
+resource "aws_route_table_association" "int_rt_subnet_db_a" {
+  subnet_id = aws_subnet.db_private_subnet_a.id
+  route_table_id = aws_route_table.internal_rt.id
 }
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_ip.id
-  connectivity_type = "public"
-  subnet_id = aws_subnet.public_subnet.id
-  tags = {
-    Name = "70491_nat"
-  }
+resource "aws_route_table_association" "int_rt_subnet_db_c" {
+  subnet_id = aws_subnet.db_private_subnet_c.id
+  route_table_id = aws_route_table.internal_rt.id
 }
 #############################################
--- variables.tf
+++ variables.tf
@@ -3,22 +3,48 @@ variable "region" {
     default = "ap-northeast-2"
 }
 
+variable "az_a" {
+  description = "first AZ"
+  default = "ap-northeast-2a"
+}
+
+variable "az_c" {
+  description = "second AZ"
+  default = "ap-northeast-2c"
+}
+
+
 variable "vpc_cidr" {
     description = "aws vpc cidr block"
     default = "10.0.0.0/16"
 }
 
-variable "public_subnet" {
+variable "public_subnet_a" {
     description = "public subnet"
     default = "10.0.1.0/24"
 }
 
-variable "ap_private_subnet" {
-    description = "ap private subnet"
+variable "public_subnet_c" {
+    description = "public subnet"
     default = "10.0.2.0/24"
 }
 
-variable "db_private_subnet" {
-    description = "db private subnet"
+variable "ap_private_subnet_a" {
+    description = "ap private subnet"
     default = "10.0.3.0/24"
+}
+
+variable "ap_private_subnet_c" {
+    description = "ap private subnet"
+    default = "10.0.4.0/24"
+}
+
+variable "db_private_subnet_a" {
+    description = "db private subnet"
+    default = "10.0.5.0/24"
+}
+
+variable "db_private_subnet_c" {
+    description = "db private subnet"
+    default = "10.0.6.0/24"
 }
