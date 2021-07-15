478099ba99983c297ffaaff596052d4a48c313b8
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-15 22:06:33 +0900

    feat: Add Auto Scaling Group and ELB

---
+++ auto-scaling.tf
@@ -0,0 +1,41 @@
+resource "aws_elb" "web_elb" {
+    name = "web-elb"
+    subnets = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_c.id]
+    instances = []
+
+    listener {
+      instance_port = 8000
+      instance_protocol = "http"
+      lb_port = 80
+      lb_protocol = "http"
+    }
+
+    tags = {
+        Name = "70491_web_elb"
+    }
+}
+
+resource "aws_launch_template" "ec2_web_template" {
+    name = "ec2_web_template"
+    description = "Web server template"
+    image_id = "ami-0b827f3319f7447c6"
+    instance_type = "t2.micro"
+    key_name = "70491_ec2_web"
+    vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+    tags = {
+        Name = "70491_ec2_web_template"
+    }
+}
+
+resource "aws_autoscaling_group" "web_autoscaling_group" {
+    name = "web_autoscaling_group"
+    min_size = 1
+    max_size = 2
+    desired_capacity = 1
+    target_group_arns = [aws_elb.web_elb.arn]
+    launch_template {
+        id = aws_launch_template.ec2_web_template.id
+    }
+    vpc_zone_identifier = [aws_subnet.ap_private_subnet_a.id, aws_subnet.ap_private_subnet_c.id]
+}
