e39c64af43bd3beac563236acd698680be87cb5f
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-15 22:26:47 +0900

    style: Delete Tags

--- auto-scaling.tf
+++ auto-scaling.tf
@@ -9,27 +9,19 @@ resource "aws_elb" "web_elb" {
       lb_port = 80
       lb_protocol = "http"
     }
-
-    tags = {
-        Name = "70491_web_elb"
-    }
 }
 
 resource "aws_launch_template" "ec2_web_template" {
-    name = "ec2_web_template"
+    name = "70491_ec2_web_template"
     description = "Web server template"
     image_id = "ami-0b827f3319f7447c6"
     instance_type = "t2.micro"
     key_name = "70491_ec2_web"
     vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-    tags = {
-        Name = "70491_ec2_web_template"
-    }
 }
 
 resource "aws_autoscaling_group" "web_autoscaling_group" {
-    name = "web_autoscaling_group"
+    name = "70491_web_autoscaling_group"
     min_size = 1
     max_size = 2
     desired_capacity = 1
