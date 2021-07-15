f2b3c446090c293a64ecae9c4688ac1d32cf5724
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-15 23:45:31 +0900

    fix: Attach EC2 to ALB

--- auto-scaling.tf
+++ auto-scaling.tf
@@ -7,6 +7,19 @@ resource "aws_lb" "web_elb" {
     
 }
 
+resource "aws_lb_target_group" "ec2_web_target_group" {
+    name = "70491-ec2-web-target-group"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "ec2_web_target_group_attach" {
+    target_group_arn = aws_lb_target_group.ec2_web_target_group.arn
+    target_id = aws_instance.ec2_web.id
+    port = 80
+}
+
 resource "aws_launch_template" "ec2_web_template" {
     name = "70491_ec2_web_template"
     description = "Web server template"
@@ -21,10 +34,11 @@ resource "aws_autoscaling_group" "web_autoscaling_group" {
     min_size = 1
     max_size = 2
     desired_capacity = 1
-    target_group_arns = [aws_lb.web_elb.arn]
+    target_group_arns = [aws_lb_target_group.ec2_web_target_group.arn]
 
     launch_template {
         id = aws_launch_template.ec2_web_template.id
+        version = "$Latest"
     }
     vpc_zone_identifier = [aws_subnet.ap_private_subnet_a.id, aws_subnet.ap_private_subnet_c.id]
 }
---
+++ ec2.tf
@@ -0,0 +1,40 @@
+################# EC2 #######################
+resource "aws_instance" "ec2_web" {
+    ami = "ami-0b827f3319f7447c6"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.ap_private_subnet_a.id 
+    availability_zone = var.az_a
+    security_groups = [aws_security_group.web_sg.id]
+    key_name = var.key_pair_ec2_web_a
+
+    root_block_device {
+        delete_on_termination = true
+        volume_size = 8
+        volume_type = "gp2"
+    }
+
+    tags = {
+        Name = "70491_ec2_web"
+    }
+}
+
+resource "aws_instance" "ec2_was"{
+    ami = "ami-0b827f3319f7447c6"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.ap_private_subnet_a.id 
+    availability_zone = var.az_a
+    security_groups = [aws_security_group.was_sg.id]
+    key_name = var.key_pair_ec2_was_a
+
+    root_block_device {
+        delete_on_termination = true
+        volume_size = 8
+        volume_type = "gp2"
+    }
+
+    tags = {
+        Name = "70491_ec2_was"
+    }
+
+}
+#############################################
