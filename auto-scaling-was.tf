1b7620961bcba8b4746734879912a22959a334db
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-16 09:33:16 +0900

    fix: Delete WAS attachment

--- auto-scaling-was.tf
+++ auto-scaling-was.tf
@@ -12,12 +12,6 @@ resource "aws_lb_target_group" "ec2_was_target_group" {
     vpc_id = aws_vpc.main_vpc.id
 }
 
-resource "aws_lb_target_group_attachment" "ec2_was_target_group_attach" {
-    target_group_arn = aws_lb_target_group.ec2_was_target_group.arn
-    target_id = aws_instance.ec2_was.id
-    port = 80  
-}
-
 resource "aws_launch_template" "ec2_was_template" {
     name = "70491_ec2_was_template"
     description = "WAS server template"
