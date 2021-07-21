ce163b37c8ba3dec164ad05be7b021added1e4eb
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-22 00:02:21 +0900

    feat: Add IAM role

---
+++ iam.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "ec2_ssm_role" {
+    name = "70491_ec2_ssm_role"
+    description = "SSM role for EC2"
+
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_role_attach" {
+    role = aws_iam_role.ec2_ssm_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
