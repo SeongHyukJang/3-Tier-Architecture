6c6614a5f170965ac44ed5312a1bb8482ad1f5c9
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-25 02:26:49 +0900

    fix: Modify WEB AMI

--- launch-template.tf
+++ launch-template.tf
@@ -1,10 +1,12 @@
 resource "aws_launch_template" "ec2_web_template" {
     name = "70491_ec2_web_template"
     description = "Web server template"
-    image_id = "ami-0b827f3319f7447c6"
+    image_id = "ami-081f3c70ff25f105b"
     instance_type = "t2.micro"
     key_name = "70491_ec2_web"
     vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+    user_data = filebase64("./run_web.sh")
 
     iam_instance_profile {
       name = aws_iam_instance_profile.ec2_iam_profile.name
---
+++ run.web.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+sudo python3 /web/server.py
