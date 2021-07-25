90cf21a476a291c58f567a50e33b0af69ea349c4
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-25 23:19:54 +0900

    fix: Modify WEB AMI

--- launch-template.tf
+++ launch-template.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_template" "ec2_web_template" {
     name = "70491_ec2_web_template"
     description = "Web server template"
-    image_id = "ami-09d52b708b31ecff9"
+    image_id = "ami-00c6c08d5bf3a8d25"
     instance_type = "t2.micro"
     key_name = "70491_ec2_web"
     vpc_security_group_ids = [aws_security_group.web_sg.id]
