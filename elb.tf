f7c51087d9c20e0d572968460b95976984719631
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-26 09:37:43 +0900

    fix: Modify PORT

--- elb.tf
+++ elb.tf
@@ -9,14 +9,14 @@ resource "aws_lb" "web_elb" {
 
 resource "aws_lb_target_group" "ec2_web_target_group" {
     name = "70491-ec2-web-target-group"
-    port = 4000
+    port = 80
     protocol = "HTTP"
     vpc_id = aws_vpc.main_vpc.id
 }
 
 resource "aws_lb_listener" "web_alb_listener" {
     load_balancer_arn = aws_lb.web_elb.arn
-    port = "4000"
+    port = "80"
     protocol = "HTTP"
 
     default_action {
