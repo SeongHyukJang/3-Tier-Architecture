4a978746807130176d672106355b27828b37f875
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-26 15:29:18 +0900

    feat: Add CloudWatch config

--- waf.tf
+++ waf.tf
@@ -25,6 +25,12 @@ resource "aws_wafv2_web_acl" "WAF_ACL" {
 
     default_action {
       allow {}
+    }
+
+    visibivisibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name = "70491-WAF"
+        sampled_requests_enabled = true
     }   
 }
 
