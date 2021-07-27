f03a7b2c493040043d14694809199c8348a619a6
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-27 15:07:19 +0900

    feat: Add CloudWatch policy

--- iam.tf
+++ iam.tf
@@ -3,7 +3,7 @@ resource "aws_iam_instance_profile" "ec2_iam_profile" {
     role = aws_iam_role.ec2_ssm_role.name
 }
 
-resource "aws_iam_role" "ec2_ssm_role" {
+resource "aws_iam_role" "70491-ec2-role" {
     name = "70491_ec2_ssm_role"
     description = "SSM role for EC2"
 
@@ -27,4 +27,9 @@ EOF
 resource "aws_iam_role_policy_attachment" "ssm_role_attach" {
     role = aws_iam_role.ec2_ssm_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_role_attach" {
+    role = aws_iam_role.70491-ec2-role.name
+    policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
