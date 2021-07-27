4ee3c816d62dd230554ee333f7edfe8db56322fe
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-27 16:35:44 +0900

    fix: Modify user data

--- run_web.sh
+++ run_web.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 python3 /web/main.py
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
