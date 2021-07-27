de81f8f82a630aa1a2fb690e57df20730ce73076
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-27 15:12:54 +0900

    fix: Modify IAM role

--- iam.tf
+++ iam.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_instance_profile" "ec2_iam_profile" {
     name = "70491_iam_profile"
-    role = aws_iam_role.ec2_ssm_role.name
+    role = aws_iam_role.ec2_role.name
 }
 
-resource "aws_iam_role" "70491-ec2-role" {
-    name = "70491_ec2_ssm_role"
-    description = "SSM role for EC2"
+resource "aws_iam_role" "ec2_role" {
+    name = "70491_ec2_role"
+    description = "role for EC2"
 
     assume_role_policy = <<EOF
 {
@@ -25,11 +25,11 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_role_attach" {
-    role = aws_iam_role.ec2_ssm_role.name
+    role = aws_iam_role.ec2_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "cloudwatch_role_attach" {
-    role = aws_iam_role.70491-ec2-role.name
+    role = aws_iam_role.ec2_role.name
     policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
