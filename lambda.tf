8189ad96bb29e20013aaf8a59e1c2cb043f62081
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-28 11:07:48 +0900

    fix: Modify to template

--- lambda.tf
+++ lambda.tf
@@ -1,13 +1,13 @@
 resource "aws_lambda_function" "log_lambda" {
-    filename = "lambda_function.zip"
+    #filename = "lambda_function.zip"
     function_name = "70491-lambda"
     role = aws_iam_role.lambda_role.arn
 
     runtime = "python3.7"
 
-    environment {
-    variables = {
-      S3_BUCKET = "70491-log-bucket"
-    }
-  }
+  #   environment {
+  #   variables = {
+  #     S3_BUCKET = "70491-log-bucket"
+  #   }
+  # }
 }
