bb3797ce64e541769f17903414552a9ac05d46b7
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-28 11:05:35 +0900

    fix: Modify lambda naming

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -22,7 +22,7 @@ resource "aws_cloudwatch_event_rule" "cloudwatch_rule_log" {
     description = "put log to s3"
 
     schedule_expression = "rate(1 minute)"
-    role_arn = aws_lambda_function.log_lambda_fuction.arn
+    role_arn = aws_lambda_function.log_lambda.arn
     is_enabled = true
 
     tags = {
