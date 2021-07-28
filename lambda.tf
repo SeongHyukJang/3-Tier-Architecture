248fb1ddc26fba77bc918b2c231edd8e321a3a7e
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-28 11:10:47 +0900

    feat: Add sha256

--- lambda.tf
+++ lambda.tf
@@ -1,9 +1,11 @@
 resource "aws_lambda_function" "log_lambda" {
-    #filename = "lambda_function.zip"
+    filename = "lambda_function.zip"
     function_name = "70491-lambda"
     role = aws_iam_role.lambda_role.arn
 
     runtime = "python3.7"
+
+    source_code_hash = filebase64sha256("lambda_function.zip")
 
   #   environment {
   #   variables = {
