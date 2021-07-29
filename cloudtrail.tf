94b1c03af9ce1b5dbb4e02cd8d877280424e68eb
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-29 10:14:52 +0900

    fix: Delete KMS

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -5,8 +5,6 @@ resource "aws_cloudtrail" "cloudtrail" {
     include_global_service_events = false
     is_multi_region_trail = false
 
-    kms_key_id = "arn:aws:kms:ap-northeast-2:533616270150:key/972c3086-0ae7-4dca-b921-8e388272846a"
-
     event_selector {
         read_write_type = "All"
         include_management_events = true
