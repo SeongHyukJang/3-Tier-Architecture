7239c57dabe70fe180c1c81a2c9bc08d7ee4a021
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-30 18:08:14 +0900

    feat: Add WAS

--- codebuild.tf
+++ codebuild.tf
@@ -45,3 +45,51 @@ resource "aws_codebuild_project" "codebuild_project_WEB" {
         ]
     }
 }
+
+
+
+resource "aws_codebuild_project" "codebuild_project_WAS" {
+    name = "70491-codebuild_WAS"
+    description = "70491-codebuild project WAS"
+    build_timeout = "5"
+    service_role = aws_iam_role.codebuild_role.arn
+    source_version = "refs/heads/master"
+
+    source {
+        type = "CODECOMMIT"
+        git_clone_depth = 1
+
+        location = "https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/70491-repo-was"   
+    }
+
+    artifacts {
+      type = "S3"
+      location = "70491-codedeploy-bucket"
+      name = "70491-codebuild_WAS"
+
+      encryption_disabled = true
+
+      namespace_type = "NONE"
+      packaging = "NONE"
+    }
+
+    environment {
+        image = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+        type = "LINUX_CONTAINER"
+        compute_type = "BUILD_GENERAL1_SMALL"
+
+    }
+
+    vpc_config {
+        vpc_id = aws_vpc.main_vpc.id
+
+        subnets = [
+            aws_subnet.ap_private_subnet_a.id,
+            aws_subnet.ap_private_subnet_c.id
+        ]
+
+        security_group_ids = [
+            aws_security_group.was_sg.id
+        ]
+    }
+}
--- codepipeline.tf
+++ codepipeline.tf
@@ -64,3 +64,74 @@ resource "aws_codepipeline" "codepipeline_WEB" {
         }
     }
 }
+
+#############################################################
+
+
+
+resource "aws_codepipeline" "codepipeline_WAS" {
+    name = "70491-codepipeline_WAS"
+    role_arn = aws_iam_role.codepipeline_role.arn
+
+    artifact_store {
+        location = aws_s3_bucket.codedeploy_bucket.bucket
+        type = "S3"
+    }
+  
+
+    stage {
+        name = "Source"
+
+        action {
+            name = "Source"
+            category = "Source"
+            owner = "AWS"
+            provider = "CodeCommit"
+            version = "1"
+            output_artifacts = ["SourceArtifact"]
+            
+            configuration = {
+              "RepositoryName" = "70491-repo-was"
+              "BranchName" = "master"
+            }
+        }
+    }
+
+    stage {
+        name = "Build"
+
+        action {
+            name = "Build"
+            category = "Build"
+            owner = "AWS"
+            provider = "CodeBuild"
+            version = "1"
+            
+            input_artifacts = ["SourceArtifact"]
+            output_artifacts = ["BuildArtifact"]
+
+            configuration = {
+              "ProjectName" = "70491-codebuild_WAS"
+            }
+        }
+    }
+
+    stage {
+        name = "Deploy"
+
+        action {
+            name = "Deploy"
+            category = "Deploy"
+            owner = "AWS"
+            provider = "CodeDeploy"
+            version = "1"
+
+            input_artifacts = ["BuildArtifact"]
+
+            configuration = {
+              "ApplicationName" = "70491-codedeploy-app_WAS"
+              "DeploymentGroupName" = "70491-codedeploy-group"
+            }
+        }
+    }
+}
