b06e012d0b74e31bbf82669a42579ca48d3e7c24
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-30 14:15:14 +0900

    fix: Delete CodeDeploy policy

--- s3.tf
+++ s3.tf
@@ -15,6 +15,4 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
 resource "aws_s3_bucket" "codedeploy_bucket" {
     bucket = "70491-codedeploy-bucket"
     acl = "private"
-
-    policy = file("codedeploy_policy.json")
 }
