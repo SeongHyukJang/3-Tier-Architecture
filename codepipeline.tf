39d70e6fdf5495b22d2102eaff1af1f57f905adb
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-30 16:40:06 +0900

    feat: Add CodePipeline

---
+++ codepipeline.tf
@@ -0,0 +1,63 @@
+resource "aws_codepipeline" "codepipeline" {
+    name = "70491-codepipeline"
+    role_arn = aws_iam_role.codepipeline_role.arn
+
+    artifact_store {
+        location = aws_s3_bucket.codedeploy_bucket.bucket
+        type = "S3"
+    }
+  
+
+    stage {
+        name = "Source"
+
+        action {
+            name = "Source"
+            category = "Source"
+            owner = "AWS"
+            provider = "CodeCommit"
+            version = "1"
+            output_artifacts = ["SourceArtifact"]
+            
+            configuration = {
+              "RepositoryName" = "70491-repo-web"
+              "BranchName" = "master"
+            }
+        }
+    }
+
+    stage {
+        name = "Build"
+
+        action {
+            name = "Build"
+            category = "Build"
+            owner = "AWS"
+            provider = "CodeBuild"
+            
+            input_artifacts = ["SourceArtifact"]
+            output_artifacts = ["BuildArtifact"]
+
+            configuration = {
+              "ProjectName" = "70491-codebuld"
+            }
+        }
+    }
+
+    stage {
+        name = "Deploy"
+
+        action {
+            name = "Deploy"
+            category = "Deploy"
+            owner = "AWS"
+            provider = "CodeDeploy"
+            input_artifacts = ["BuildArtifact"]
+
+            configuration = {
+              "ApplicationName" = "70491-deploy"
+              "DeploymentGroupName" = "70491-codedeploy-group"
+            }
+        }
+    }
+}
