13bcfaaf4c9845a5444701bf46c08779a9cda8ac
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-30 15:46:27 +0900

    fix: Modify target group info

--- codedeploy.tf
+++ codedeploy.tf
@@ -5,7 +5,7 @@ resource "aws_codedeploy_app" "codedeploy_app" {
 
 resource "aws_codedeploy_deployment_group" "codedeploy_group" {
     app_name = aws_codedeploy_app.codedeploy_app.name
-    deployment_group_name = "70491-group"
+    deployment_group_name = "70491-codedeploy-group"
     deployment_config_name = "CodeDeployDefault.AllAtOnce"
     service_role_arn = aws_iam_role.codedeploy_role.arn
 
@@ -15,7 +15,7 @@ resource "aws_codedeploy_deployment_group" "codedeploy_group" {
     }
     load_balancer_info {
         target_group_info {
-            name = aws_lb_target_group.ec2_web_target_group.arn
+            name = aws_lb_target_group.ec2_web_target_group.name
         }
       
     }
