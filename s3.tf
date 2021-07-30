bcd299cb5bba00b1b35fe1d9bfba4321cc0b083b
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-30 12:00:45 +0900

    feat: Add CodeDeploy Bucket

--- s3.tf
+++ s3.tf
@@ -11,3 +11,10 @@ resource "aws_s3_bucket" "cloudtrail_bucket" {
 
     policy = file("cloudtrail_policy.json")
 }
+
+resource "aws_s3_bucket" "codedeploy_bucket" {
+    bucket = "70491-codedeploy-bucket"
+    acl = "private"
+
+    policy = file("codedeploy_policy.json")
+}
