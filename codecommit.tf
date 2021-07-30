52b1ff22697cae059f95f45f55abb8ec742d4e81
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-30 09:35:08 +0900

    feat: Add WAS repo

--- codecommit.tf
+++ codecommit.tf
@@ -1,4 +1,9 @@
-resource "aws_codecommit_repository" "code_repo" {
-    repository_name = "70491-repo"
-    description = "Web APP repo"
+resource "aws_codecommit_repository" "code_repo_web" {
+    repository_name = "70491-repo-web"
+    description = "WEB repo"
+}
+
+resource "aws_codecommit_repository" "code_repo_was" {
+    repository_name = "70491-repo-was"
+    description = "WAS repo"
 }
