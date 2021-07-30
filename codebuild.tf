0760f4f7dabe8b1d5d621e68ff21f71f992d2905
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-30 15:56:00 +0900

    feat: Add source version

--- codebuild.tf
+++ codebuild.tf
@@ -3,6 +3,7 @@ resource "aws_codebuild_project" "codebuild_project" {
     description = "70491-codebuild project"
     build_timeout = "5"
     service_role = aws_iam_role.codebuild_role.arn
+    source_version = "master"
 
     source {
         type = "CODECOMMIT"
