7b7a94b04401335d5d6ac32bbae34e9b1c3ad831
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-07-31 15:54:55 +0900

    feat: Add CodeDeploy

--- run_web.sh
+++ run_web.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
+/install auto
+service codedeploy-agent start
+
 python3 /web/main.py
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
-
-yum install -y ruby
-yum install wget
-wget https://aws-codedeploy-ap-northeast-2.s3.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
