5d462bf34b3594e0af57da1dbb6bfe9956a51982
SeongHyukJang <seonghyuk578@khu.ac.kr>
2021-08-03 16:27:19 +0900

    fix: Modify cluster to private

--- eks.tf
+++ eks.tf
@@ -3,12 +3,10 @@ resource "aws_eks_cluster" "eks_cluster" {
     role_arn = aws_iam_role.eks_role.arn
 
     vpc_config {
-        endpoint_public_access = true
-        endpoint_private_access = false
+        endpoint_public_access = false
+        endpoint_private_access = true
 
-        public_access_cidrs = ["0.0.0.0/0"]
-
-        subnet_ids = [  aws_subnet.public_subnet_a.id,      aws_subnet.public_subnet_c.id, 
+        subnet_ids = [  aws_subnet.public_subnet_a.id,      aws_subnet.public_subnet_c.id,
                         aws_subnet.ap_private_subnet_a.id,  aws_subnet.ap_private_subnet_c.id]
     }
 }
